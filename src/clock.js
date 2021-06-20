57e8a91afc6381099b193847f9150544bebc4bfa
kulisk <kirillka.kulikov@mail.ru>
2021-06-20 22:47:45 +0400

    fixes

--- src/clock.js
+++ src/clock.js
@@ -1,18 +1,14 @@
 export function Clock(id) {
-    this.id = id;
+    let clock = document.getElementById(id);
 
     this.start = function () {
-        let clockTag = document.createElement('div');
-        clockTag.id = id;
-        document.body.append(clockTag);
-        this.clock = document.getElementById(this.id);
-        clockTag.innerHTML = this.tick();
+        this.tick();
         setInterval(this.tick, 1000);
     }
 
     this.tick = function () {
         let date = new Date();
         let halfDay = (date.getHours() >= 12) ? 'PM' : 'AM';
-        return this.clock.innerHTML = `${date.getHours() % 12}:${date.getMinutes()}:${date.getSeconds()} ${halfDay}`;
+        clock.textContent = `${date.getHours() % 12}:${date.getMinutes()}:${date.getSeconds()} ${halfDay}`;
     }
 }

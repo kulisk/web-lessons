c97b430c7c26f48faa8d3912b372bfc6a365c9ed
kulisk <kirillka.kulikov@mail.ru>
2021-06-25 12:33:08 +0400

    timeline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "../styles/main.css";
+
+function onPageLoaded() {
+    let items = document.querySelectorAll(".timeline li");
+    items = Array.from(items);
+
+    function isElementInViewport(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function checkItems() {
+        let itemsToDelete = [];
+        for (let i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+                itemsToDelete.push(i);
+            }
+        }
+        for (let i = itemsToDelete.length - 1; i >= 0; i--) {
+            items.splice(itemsToDelete[i], 1);
+        }
+    }
+
+    window.addEventListener("load", checkItems);
+    window.addEventListener("resize", checkItems);
+    window.addEventListener("scroll", checkItems);
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded);

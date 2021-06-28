bff0bcf274c1f1dc8ae0d8f0b6a2dfcb0af352f2
kulisk <kirillka.kulikov@mail.ru>
2021-06-28 13:42:39 +0400

    observer api

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
 import "../styles/main.css";
 
-function onPageLoaded() {
-    let items = document.querySelectorAll(".timeline li");
-    items = Array.from(items);
-
-    function isElementInViewport(el) {
-        let rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    function checkItems() {
-        let itemsToDelete = [];
-        for (let i = 0; i < items.length; i++) {
-            if (isElementInViewport(items[i])) {
-                items[i].classList.add("in-view");
-                itemsToDelete.push(i);
-            }
-        }
-        for (let i = itemsToDelete.length - 1; i >= 0; i--) {
-            items.splice(itemsToDelete[i], 1);
-        }
-    }
-
-    window.addEventListener("load", checkItems);
-    window.addEventListener("resize", checkItems);
-    window.addEventListener("scroll", checkItems);
+let options = {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '0px',
+    threshold: 1.0
 }
 
-document.addEventListener("DOMContentLoaded", onPageLoaded);
+let observer = new IntersectionObserver(checkItems, options);
+document.querySelectorAll(".timeline li").forEach(item => observer.observe(item));
+
+function checkItems(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            observer.unobserve(entry.target);
+        }
+    })
+}

6167a6723a0623bae142162dd8cd83ae4f03234c
kulisk <kirillka.kulikov@mail.ru>
2021-07-07 14:23:17 +0400

    timeline

---
+++ src/const/timestamps.js
@@ -0,0 +1,50 @@
+export const timestamps = [
+    {
+        time: '1934',
+        text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At veroeos etaccusamus et iusto odio dignissimos ducimus qui blanditiis praesentium'
+    },
+
+    {
+        time: '1937',
+        text: 'Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris etbibendum molestie.Aenean ex augue, varius et pulvinar in, pretium non nisi.'
+    },
+    {
+        time: '1940',
+        text: 'Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex egetligula. Integer eu dapibus arcu, sit amet sollicitudin eros.'
+    },
+    {
+        time: '1943',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '1946',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '1956',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '1957',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '1967',
+        text: 'Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus,euismodquis ante.'
+    },
+    {
+        time: '1977',
+        text: 'Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quamdolor convallis enim, non dignissim ligula ipsum a turpis.'
+    }, {
+        time: '1985',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '2000',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+    {
+        time: '2005',
+        text: 'In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis.Vestibulum id magna est.'
+    },
+]
